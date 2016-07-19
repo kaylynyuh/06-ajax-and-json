6ce66bf2ce7962fa76c9b471a400eaa507ad38ed
kaylynyuh <yuhkay4@gmail.com>
2016-07-18 19:50:13 -0700

    TODOs done

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -4,7 +4,7 @@ function Article (opts) {
   }
 }
 
-/* TODO: Instead of a global `articles = []` array, let's track this list of all
+/* TODO: DONE Instead of a global `articles = []` array, let's track this list of all
  articles directly on the constructor function. Note: it is NOT on the prototype.
  In JavaScript, functions are themselves objects, which means we can add
  properties/values to them at any time. In this case, we have a key:value pair
@@ -14,6 +14,7 @@ function Article (opts) {
 
 Article.all = [];
 
+
 Article.prototype.toHtml = function(scriptTemplateId) {
   var template = Handlebars.compile($(scriptTemplateId).text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
@@ -22,12 +23,13 @@ Article.prototype.toHtml = function(scriptTemplateId) {
   return template(this);
 };
 
+
 /* NOTE: There are some other functions that also relate to all articles,
  rather than just single instances. Object-oriented programming would
  call these "class-level" functions, that are relevant to the entire "class"
  of objects that are Articles, rather than just one instance. */
 
-/* TODO: Refactor this code into a function for greater control.
+/* TODO: DONE Refactor this code into a function for greater control.
     It will take in our data, and process it via the Article constructor: */
 
 Article.loadAll = function(dataWePassIn) {
@@ -46,10 +48,21 @@ Article.fetchAll = function() {
     /* When our data is already in localStorage:
     1. We can process it (sort and instantiate),
     2. Then we can render the index page. */
-    // Article.loadAll(// TODO: Invoke with our localStorage! Should we parse or stringify this?);
-    // TODO: Now let's render the index page.
+    // Article.loadAll(// TODO: DONE Invoke with our localStorage! Should we parse or stringify this?);
+    // TODO: DONE Now let's render the index page.
+    Article.loadAll(JSON.parse(localStorage.hackerIpsum));
   } else {
-    /* TODO: Otherwise, without our localStorage data, we need to:
+    // Load our json data
+    $.ajax({
+      type: 'GET',
+      url: '/data/hackerIpsum.json'
+    }).done(function(data) {
+      // Store that data in localStorage so we can skip the server call next time
+      localStorage.hackerIpsum = JSON.stringify(data);
+    });
+
+
+    /* TODO: DONE Otherwise, without our localStorage data, we need to:
     - Retrive our JSON file asynchronously
      (which jQuery method method is best for this?).
      Within this method, we should:
@@ -60,7 +73,10 @@ Article.fetchAll = function() {
 };
 
 
-
+Article.fetchAll();
+Article.all.forEach(function(generateNewArticles) {
+  $('#articles').append(generateNewArticles.toHtml('#article-template'));
+});
 
 
 
